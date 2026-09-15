efcec4dab4160e95a05dc5313e97316a96dad206
Chengda Wang <168561125+akjncjancj@users.noreply.github.com>
2026-09-15 17:43:23 +0800

    Update llada_15_instruct_8b.py

--- SAID-block/opencompass/opencompass/configs/models/dllm/llada_15_instruct_8b.py
+++ SAID-block/opencompass/opencompass/configs/models/dllm/llada_15_instruct_8b.py
@@ -4,7 +4,7 @@ models = [
     dict(
         type=LLaDAModel,
         abbr='llada-1.5-8b-instruct',
-        path='GSAI-ML/LLaDA-1.5',  # 服务器上改成本地路径，如 /home/ubuntu/.cache/huggingface/hub/models--GSAI-ML--LLaDA-1.5/snapshots/xxx
+        path='GSAI-ML/LLaDA-1.5',  
         max_out_len=1024,
         batch_size=1,
         run_cfg=dict(num_gpus=1),
